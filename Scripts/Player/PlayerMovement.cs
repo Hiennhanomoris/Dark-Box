012d6133e0e0a685181c0a9ec6b85fa5fadc3de0
Phúc Perseids <61786162+Hiennhanomoris@users.noreply.github.com>
2022-02-13 23:30:02 +0700

    jumping

--- Scripts/Player/PlayerMovement.cs
+++ Scripts/Player/PlayerMovement.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float move_speed;
-    [SerializeField] private float force;
     private bool facingRinght;
     private bool isOnGround;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask whatIsGround;
     [SerializeField] private float checkRadius;
+    public PlayerSO playerStatus;
 
     Rigidbody2D player_rb;
 
@@ -18,12 +17,40 @@ public class PlayerMovement : MonoBehaviour
     {
         player_rb = GetComponent<Rigidbody2D>();    
         facingRinght = true;
+
+        playerStatus.currentHealth = playerStatus.maxHealth;
+        playerStatus.currentJump = playerStatus.extraJump;
     }
 
     private void FixedUpdate() 
     {
         HandleMovement();
         CheckOnGround();
+    }
+
+    private void Update() 
+    {
+        HandleJumping();
+    }
+
+    private void HandleJumping()
+    {
+        if(isOnGround == true)
+        {
+            //reset current jump when player stand on the ground
+            playerStatus.currentJump = playerStatus.extraJump;
+        }
+
+        //player jumping when press key
+        if(Input.GetKeyDown(KeyCode.Space) && playerStatus.currentJump > 0)
+        {
+            player_rb.velocity = Vector2.up * playerStatus.jumpForce;
+            playerStatus.currentJump -= 1;
+        }
+        else if(Input.GetKeyDown(KeyCode.Space) && playerStatus.currentJump > 0 && isOnGround)
+        {
+            player_rb.velocity = Vector2.up * playerStatus.jumpForce;
+        }
     }
 
     private void CheckOnGround()
@@ -33,9 +60,9 @@ public class PlayerMovement : MonoBehaviour
 
     private void HandleMovement()
     {
+        //move player 
         float horizontal = Input.GetAxis("Horizontal");
-        Vector2 movement = new Vector2(horizontal, 0);
-        player_rb.velocity = movement * move_speed;
+        player_rb.velocity = new Vector2(horizontal * playerStatus.moveSpeed, player_rb.velocity.y);
 
         if((facingRinght == false && horizontal > 0) || (facingRinght == true && horizontal < 0))
         {
@@ -45,6 +72,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Flip()
     {
+        //change direction of this object
         facingRinght = !facingRinght;
         Vector3 scaler = this.transform.localScale;
         scaler.x *= -1;
