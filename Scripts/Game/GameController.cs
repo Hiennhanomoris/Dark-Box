40be7e08921897455da1a64ad08c1fecd0bf7e3d
Pegasus <hiennhanomoris2001@gmail.com>
2022-02-26 21:54:12 +0700

    add enemy2 to prefabs

--- Scripts/Game/GameController.cs
+++ Scripts/Game/GameController.cs
@@ -7,6 +7,9 @@ public class GameController : MonoBehaviour
     public static GameController Instance;
     [SerializeField] private GameObject enemy1;
     [SerializeField] private Transform[] enemy1SpawnTranform;
+    [SerializeField] private GameObject enemy2;
+    [SerializeField] private Transform[] enemy2SpawnTranform;
+
 
     private void Awake() 
     {
@@ -20,7 +23,8 @@ public class GameController : MonoBehaviour
 
     private void Start() 
     {
-        StartCoroutine(SpawnEnemy1());    
+        StartCoroutine(SpawnEnemy1());  
+        StartCoroutine(SpawnEnemy2());  
     }
 
     private IEnumerator SpawnEnemy1()
@@ -29,6 +33,17 @@ public class GameController : MonoBehaviour
         for(int i = 0; i < enemy1SpawnTranform.Length; i++)
         {
             Instantiate(enemy1, enemy1SpawnTranform[i].position, Quaternion.identity);
+
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    private IEnumerator SpawnEnemy2()
+    {
+        yield return new WaitForSeconds(1f);
+        for(int i = 0; i < enemy2SpawnTranform.Length; i++)
+        {
+            Instantiate(enemy2, enemy2SpawnTranform[i].position, Quaternion.identity);
 
             yield return new WaitForSeconds(2f);
         }
